353c63ca1b39d1f806fbd9ac2e8c0509356e0158
MrILL <ilua2403@ukr.net>
2024-05-14 21:32:44 +0300

    feat: adjust debug panel

--- Scenes/PlayerStateMachine/playerStateMachine.gd
+++ Scenes/PlayerStateMachine/playerStateMachine.gd
@@ -1,5 +1,6 @@
 extends CharacterBody3D
 class_name Player
 
-
-
+func _process(delta):
+	$DebugPanelInterface.upsertDebugProperty("FPS", "%.2f" % (1.0/delta))
+	$DebugPanelInterface.upsertDebugProperty("State", $FiniteStateMachine.current_state.name.to_lower())
