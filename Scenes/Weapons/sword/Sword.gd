18bc576eeced9ba19a455b4f891b3dd3c16f43b5
Bessonica <studies.vuz@gmail.com>
2024-05-18 18:06:43 +0300

    attack functionality, block/attack area3D

--- Scenes/Weapons/sword/Sword.gd
+++ Scenes/Weapons/sword/Sword.gd
@@ -3,9 +3,16 @@ class_name Sword
 
 
 @onready var animationPlayer = $AnimationPlayer
+@onready var attackArea = $AttackArea
+@onready var blockArea = $BlockArea
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	attackArea.monitorable = false
+	attackArea.monitoring = false
+	
+	blockArea.monitorable = false
+	blockArea.monitoring = false
 	#await get_tree().create_timer(5.0).timeout
 	#animationPlayer.play("Idle")
 	pass # Replace with function body.
@@ -17,13 +24,21 @@ func _process(delta):
 
 
 func attack():
+	attackArea.monitoring = true
 	animationPlayer.play("Attack")
 
 func block():
 	animationPlayer.play("Block")
+	blockArea.monitoring = true
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "Attack":
 		animationPlayer.play("Idle")
+		attackArea.monitoring = false
 	if anim_name == "Block":
 		animationPlayer.play("Idle")
+		blockArea.monitoring = false
+
+
+func _on_attack_area_body_entered(body):
+	body.gotHit()
